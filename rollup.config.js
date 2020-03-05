7c57df5dd7fa3c21f2f73edca988013e84548200
Shreyas Minocha <sm-git@riseup.net>
2020-03-05 20:06:06 +0530

    Run prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,62 +1,66 @@
-import css from 'rollup-plugin-css-only';
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import {terser} from 'rollup-plugin-terser';
+import css from "rollup-plugin-css-only";
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js',
-		globals: {
-			'd3-color-difference': 'ciede2000',
-			convert: 'color-convert'
-		},
-	},
-	plugins: [
-		css({ output: "public/build/extra.css" }),
+    input: "src/main.js",
+    output: {
+        sourcemap: true,
+        format: "iife",
+        name: "app",
+        file: "public/build/bundle.js",
+        globals: {
+            "d3-color-difference": "ciede2000",
+            convert: "color-convert"
+        }
+    },
+    plugins: [
+        css({ output: "public/build/extra.css" }),
 
-		svelte({
-			dev: !production,
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
-		}),
+        svelte({
+            dev: !production,
+            css: css => {
+                css.write("public/build/bundle.css");
+            }
+        }),
 
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
+        resolve({
+            browser: true,
+            dedupe: ["svelte"]
+        }),
+        commonjs(),
 
-		!production && serve(),
-		!production && livereload('public'),
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+        !production && serve(),
+        !production && livereload("public"),
+        production && terser()
+    ],
+    watch: {
+        clearScreen: false
+    }
 };
 
 function serve() {
-	let started = false;
+    let started = false;
 
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
+    return {
+        writeBundle() {
+            if (!started) {
+                started = true;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
+                require("child_process").spawn(
+                    "npm",
+                    ["run", "start", "--", "--dev"],
+                    {
+                        stdio: ["ignore", "inherit", "inherit"],
+                        shell: true
+                    }
+                );
+            }
+        }
+    };
 }
