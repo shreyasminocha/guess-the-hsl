6f3ab52b129274077b25fcb97ade5e6a95dbfcad
Shreyas Minocha <sm-git@riseup.net>
2020-03-06 13:33:58 +0530

    Extract slider into a separate component

--- src/app.svelte
+++ src/app.svelte
@@ -1,5 +1,6 @@
 <script>
     import Swatch from "./components/swatch.svelte";
+    import Slider from "./components/slider.svelte";
     import Results from "./components/results.svelte";
     import random from "random-int";
 
@@ -39,31 +40,6 @@
     .main {
         margin-top: 5em;
     }
-
-    fieldset {
-        margin: 0;
-        margin-bottom: 2em;
-    }
-
-    legend {
-        width: 100%;
-    }
-
-    input {
-        width: unset;
-    }
-
-    input[type="range"],
-    input[type="number"] {
-        display: block;
-        margin: 0 auto;
-    }
-
-    input[type="number"] {
-        width: 7ch;
-        border: none;
-        text-align: center;
-    }
 </style>
 
 <h1>Guess the HSL</h1>
@@ -75,62 +51,17 @@
     <div class="main">
         <Swatch color={solution} inline={false} />
 
-        <fieldset>
-            <legend>Hue</legend>
-
-            <input
-                type="range"
-                name="hue"
-                bind:value={input[0]}
-                min="0"
-                max="360"
-                step="1" />
-            <input
-                type="number"
-                name="hue"
-                bind:value={input[0]}
-                min="0"
-                max="360"
-                step="1" />
-        </fieldset>
-
-        <fieldset>
-            <legend>Saturation</legend>
-
-            <input
-                type="range"
-                name="saturation"
-                bind:value={input[1]}
-                min="0"
-                max="100"
-                step="1" />
-            <input
-                type="number"
-                name="saturation"
-                bind:value={input[1]}
-                min="0"
-                max="100"
-                step="1" />
-        </fieldset>
-
-        <fieldset>
-            <legend>Lightness</legend>
-
-            <input
-                type="range"
-                name="lightness"
-                bind:value={input[2]}
-                min="0"
-                max="100"
-                step="1" />
-            <input
-                type="number"
-                name="lightness"
-                bind:value={input[2]}
-                min="0"
-                max="100"
-                step="1" />
-        </fieldset>
+        <Slider name="Hue" bind:value={input[0]} range={[0, 360]} step={1} />
+        <Slider
+            name="Saturation"
+            bind:value={input[1]}
+            range={[0, 100]}
+            step={1} />
+        <Slider
+            name="Lightness"
+            bind:value={input[2]}
+            range={[0, 100]}
+            step={1} />
     </div>
 
     <input type="submit" on:click={save} value="Submit" />
---
+++ src/components/slider.svelte
@@ -0,0 +1,52 @@
+<script>
+    export let name;
+    export let value;
+    export let range = [0, 100];
+    export let step = 1;
+</script>
+
+<style>
+    fieldset {
+        margin: 0;
+        margin-bottom: 2em;
+    }
+
+    legend {
+        width: 100%;
+    }
+
+    input {
+        width: unset;
+    }
+
+    input[type="range"],
+    input[type="number"] {
+        display: block;
+        margin: 0 auto;
+    }
+
+    input[type="number"] {
+        width: 7ch;
+        border: none;
+        text-align: center;
+    }
+</style>
+
+<fieldset>
+    <legend>{name}</legend>
+
+    <input
+        type="range"
+        {name}
+        bind:value
+        min={range[0]}
+        max={range[1]}
+        {step} />
+    <input
+        type="number"
+        name="hue"
+        bind:value
+        min={range[0]}
+        max={range[1]}
+        {step} />
+</fieldset>
